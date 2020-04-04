82b200df3d0c64dc5275c83c56e8bf967dd7c298
TheInfinity007 <ramchandra71999@gmail.com>
2020-04-04 13:32:16 +0530

    add the stylesheet

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get("/", (req, res)=>{
 	res.render("search");
 });
 
+//index route
 app.get("/search", (req, res)=>{
 	let query = req.query;
 	console.log(req._parsedUrl.query);
@@ -42,23 +43,57 @@ app.get("/search", (req, res)=>{
 	});
 });
 
+var seedData = {
+  Title: 'Jumanji: Welcome to the Jungle',
+  Year: '2017',
+  Rated: 'PG-13',
+  Released: '20 Dec 2017',
+  Runtime: '119 min',
+  Genre: 'Action, Adventure, Comedy, Fantasy',
+  Director: 'Jake Kasdan',
+  Writer: 'Chris McKenna (screenplay by), Erik Sommers (screenplay by), Scott Rosenberg (screenplay by), Jeff Pinkner (screenplay by), Chris McKenna (screen story by), Chris Van Allsburg (based on the book "Jumanji" by), Greg Taylor (based on the film "Jumanji" screen story by), Chris Van Allsburg (based on the film "Jumanji" screen story by), Jonathan Hensleigh (based on the film "Jumanji" screenplay by), Greg Taylor (based on the film "Jumanji" screenplay by), Jim Strain (based on the film "Jumanji" by)',
+  Actors: 'Dwayne Johnson, Kevin Hart, Jack Black, Karen Gillan',
+  Plot: 'Four teenagers are sucked into a magical video game, and the only way they can escape is to work together to finish the game.',
+  Language: 'English',
+  Country: 'USA, India, Canada, UK, Australia, Germany',
+  Awards: '5 wins & 14 nominations.',
+  Poster: 'https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+  Ratings: [
+    { Source: 'Internet Movie Database', Value: '6.9/10' },
+    { Source: 'Rotten Tomatoes', Value: '76%' },
+    { Source: 'Metacritic', Value: '58/100' }
+  ],
+  Metascore: '58',
+  imdbRating: '6.9',
+  imdbVotes: '289,618',
+  imdbID: 'tt2283362',
+  Type: 'movie',
+  DVD: '20 Mar 2018',
+  BoxOffice: '$393,201,353',
+  Production: 'Columbia Pictures',
+  Website: 'N/A',
+  Response: 'True'
+};
+
+//show route
 app.get("/search/:imdbID", (req, res)=>{
 		let url = "http://www.omdbapi.com/?i=" + req.params.imdbID + "&apikey=" + process.env.OMDB_API_KEY;
 		console.log(url);
-		request(url, (error, response, body)=>{
-			if(!error && response.statusCode == 200){
-				let data = JSON.parse(body);
-				console.log(response.statusCode);
-				console.log(data);
-				if(data.Response == "True"){
-					res.render("show", {data: data});
-				}else{
-					res.send(data);
-				}
-			}else{
-				res.send("Error Occured! Please Try again");
-			}
-		});
+		// request(url, (error, response, body)=>{
+		// 	if(!error && response.statusCode == 200){
+		// 		let data = JSON.parse(body);
+		// 		console.log(response.statusCode);
+		// 		console.log(data);
+		// 		if(data.Response == "True"){
+		// 			res.render("show", {data: data});
+		// 		}else{
+		// 			res.send(data);
+		// 		}
+		// 	}else{
+		// 		res.send("Error Occured! Please Try again");
+		// 	}
+		// });
+		res.render("show", {data: seedData});
 });
 
 app.listen(3000, ()=>{
